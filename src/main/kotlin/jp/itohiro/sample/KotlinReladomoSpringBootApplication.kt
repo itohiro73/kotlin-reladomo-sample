d72093d2a9214db3a8a7e7bdf1723c9d01229403
Hiroshi Ito <itohiro73@gmail.com>
2024-04-22 18:53:14 +0900

    Initial commit.

---
+++ src/main/kotlin/jp/itohiro/sample/KotlinReladomoSpringBootApplication.kt
@@ -0,0 +1,11 @@
+package jp.itohiro.sample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinReladomoSpringBootApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinReladomoSpringBootApplication>(*args)
+}
